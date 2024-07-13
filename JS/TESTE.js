5506433d6f5a15b1c6865da8a280374b995e991c
Felipe R. <feliperamos0424@hotmail.com>
2024-07-13 12:49:08 -0300

    html

--- JS/TESTE.js
+++
@@ -1,7 +0,0 @@
-/* 15. Declare uma variável chamada `counter` e atribua a ela o valor 0. Use um loop `do...while` para incrementar o valor de `counter` até que ele seja igual a 5, imprimindo o valor de `counter` a cada iteração. */
-
-var counter = 0;
-while (counter < 5) {
-    counter++
-    console.log(counter);
-}
